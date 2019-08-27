6d9d2b845862c4371db39b7d72b382307dfa8a3a
candy12t <candy12t@gmail.com>
2019-08-28 00:32:22 +0900

    修正

--- ABC086C.py
+++ ABC086C.py
@@ -1,7 +1,7 @@
 N = int(input())
 for i in range(N):
   t,x,y = map(int, input().split())
-  if x + y > t or (t+x+y) % 2:
+  if (x + y) > t or (t+x+y) % 2:
     print('No')
     exit()
-print('YES')
+print('Yes')

4f54eda11ddc872ff91f46565e7f600645fb863e
candy12t <candy12t@gmail.com>
2020-01-11 01:31:46 +0900

    C問題完了!

--- 2020_1_10/C.py
+++ 2020_1_10/C.py
@@ -1,3 +1,6 @@
+import itertools # 順列生成
 N = int(input())
-P_list = list(map(int, input().split()))
-Q_list = list(map(int, input().split()))
+P_list = tuple(map(int, input().split()))
+Q_list = tuple(map(int, input().split()))
+raw = list(itertools.permutations(range(1, N+1)))
+print(abs(raw.index(P_list) - raw.index(Q_list)))
